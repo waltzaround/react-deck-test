e1d5f3f15f2322b74609d10fc1944b8f9d8be323
Walter Lim <waltissomewhere@gmail.com>
2020-03-01 16:19:36 +1300

    import and play with material

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 /// app.js
 import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+import styled from 'styled-components'
 
+const BodyPotato = styled.body`
+font-size: 12rem;
+`
 
 export default class App extends React.Component {
   render() {
 
 
     return (
-   <div></div>
+      <html>
+        <meta
+  name="viewport"
+  content="minimum-scale=1, initial-scale=1, width=device-width"
+/>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+   <header>potato</header>
+   <BodyPotato>carrot</BodyPotato>
+   <Button variant="contained" color="primary">Potatoes</Button>
+   <footer>peas</footer>
+   </html>
     );
   }
 }
